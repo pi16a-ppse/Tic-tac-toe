db91465038ed0781023d38175da1290e7779dcde
AnnaSerozhenko <annieserezhenko@gmail.com>
2019-05-16 14:33:29 +0300

    Добавление коммитов

--- script.js
+++ script.js
@@ -4,14 +4,25 @@ var znak_comp = 'O';
 var score_user = '0';
 var score_comp = '0';
 
+/**
+ * Инициализация
+ * @returns {void}
+ */
 $(document).ready(function(){
-    //Увеличение на 130%
+    /*
+     * Увеличение на 130%
+     */
     document.body.style.zoom = "130%";
 
     var exit_flag = false;
     var win_user_array = ['123','456','789','147','258','369','159','357'];
          
-    //Определяем победу игрока
+    /**
+    *Определяем победу игрока
+    *
+    *@param {integer} znak - знак Х
+    *@return {integer} +1 к счёту игрока
+    */
     function check3user(znak){
         for (var i = 0; i < 8; i++) {
          
@@ -19,6 +30,9 @@ $(document).ready(function(){
             var second = 'kletka' + win_user_array[i].substr(1,1);
             var third = 'kletka' + win_user_array[i].substr(2,1);
              
+             /**
+             * Проверка выигрыша игрока
+             */ 
             if( $('.'+first).text() == znak && $('.'+second).text() == znak && $('.'+third).text() == znak ){
                 $('.kletka1, .kletka2, .kletka3, .kletka4, .kletka5, .kletka6, .kletka7, .kletka8, .kletka9').css("background-color", "#FFF");
                 $('.'+first+',.'+second+',.'+third).css("background-color", "#83e2c3");
@@ -31,7 +45,12 @@ $(document).ready(function(){
         }
     }
      
-    //Определяем возможность победы компьютера
+     /**
+    *Определяем возможность победы компьютера
+    *
+    *@param {integer} znak - знак О
+    *@return {integer} +1 к счёту компьютера
+    */
     function check2comp(znak){
         for (var i = 0; i < 8; i++) {
          
@@ -39,6 +58,9 @@ $(document).ready(function(){
             var second = 'kletka' + win_user_array[i].substr(1,1);
             var third = 'kletka' + win_user_array[i].substr(2,1);
              
+             /**
+             * Проверка выигрыша коспьютера
+             */ 
             if( $('.'+first).text() == znak && $('.'+second).text() == znak && $('.'+third).text() == '' && exit_flag == false ){
                 $('.'+third).text(znak);
                 $('.'+first+',.'+second+',.'+third).css("background-color", "#EF7C7C");
@@ -71,7 +93,11 @@ $(document).ready(function(){
         }
     }
      
-    //Определяем ход компьютера
+    /**
+    *Определяем ход компьютера
+    *
+    *@param {integer} znak - знак О
+    */
     function check2user(znak){
  
         for (var i = 0; i < 8; i++) {
@@ -104,6 +130,9 @@ $(document).ready(function(){
         }
     }
      
+    /**
+    * Обработка клика ☺ + ☻™ = ♥♥♥
+    */  
     $('.MAIN_DIV .div').click(function(){
         //Если клетка пустая
         if( $(this).text() == '' ){
@@ -124,13 +153,18 @@ $(document).ready(function(){
     });
 });
 
+/**
+*Перезагрузка игры
+*/
 function restart(){
     $('.result').text('Ваш ход!');
     $('.kletka1, .kletka2, .kletka3, .kletka4, .kletka5, .kletka6, .kletka7, .kletka8, .kletka9').text('');
     $('.kletka1, .kletka2, .kletka3, .kletka4, .kletka5, .kletka6, .kletka7, .kletka8, .kletka9').css("background-color", "#FFF");
     $( ".MAIN_DIV .div" ).bind("click", function (){
  
-        //Если клетка пустая
+        /**
+        * Если клетка пустая
+        */ 
         if( $(this).text() == '' ){
             $(this).text(znak_user);    
             check3user(znak_user);
@@ -152,7 +186,12 @@ function restart(){
 var exit_flag = false;
 var win_user_array = ['123','456','789','147','258','369','159','357'];
      
-//Определяем победу игрока
+/**
+*Определяем победу игрока
+*
+*@param {integer} znak - знак Х
+*@return {integer} +1 к счёту игрока
+*/
 function check3user(znak){
     for (var i = 0; i < 8; i++) {
         check();
@@ -160,6 +199,9 @@ function check3user(znak){
         var second = 'kletka' + win_user_array[i].substr(1,1);
         var third = 'kletka' + win_user_array[i].substr(2,1);
          
+         /**
+         * Проверка выигрыша игрока
+         */ 
         if( $('.'+first).text() == znak && $('.'+second).text() == znak && $('.'+third).text() == znak ){
             $('.kletka1, .kletka2, .kletka3, .kletka4, .kletka5, .kletka6, .kletka7, .kletka8, .kletka9').css("background-color", "#FFF");
             $('.'+first+',.'+second+',.'+third).css("background-color", "#83e2c3");
@@ -172,7 +214,12 @@ function check3user(znak){
     }
 }
  
-//Определяем возможность победы компьютера
+/**
+*Определяем возможность победы компьютера
+*
+*@param {integer} znak - знак О
+*@return {integer} +1 к счёту компьютера
+*/
 function check2comp(znak){
     for (var i = 0; i < 8; i++) {
      
@@ -180,6 +227,9 @@ function check2comp(znak){
         var second = 'kletka' + win_user_array[i].substr(1,1);
         var third = 'kletka' + win_user_array[i].substr(2,1);
          
+        /**
+        * Проверка выигрыша коспьютера
+        */ 
         if( $('.'+first).text() == znak && $('.'+second).text() == znak && $('.'+third).text() == '' && exit_flag == false ){
             $('.'+third).text(znak);
             $('.'+first+',.'+second+',.'+third).css("background-color", "#EF7C7C");
@@ -212,7 +262,11 @@ function check2comp(znak){
     }
 }
  
-//Определяем ход компьютера
+/**
+*Определяем ход компьютера
+*
+*@param {integer} znak - знак О
+*/
 function check2user(znak){
 
     for (var i = 0; i < 8; i++) {
@@ -243,7 +297,10 @@ function check2user(znak){
         if(exit_flag) break;
     }
 }
- 
+
+/**
+*Обработка клика ☺ + ☻™ = ♥♥♥
+*/ 
 $('.MAIN_DIV .div').click(function(){
     if( $(this).text() == '' ){
         $(this).text(znak_user); 
@@ -262,6 +319,9 @@ $('.MAIN_DIV .div').click(function(){
     }
 });
 
+/**
+*Подсветка желтым цветом поля, если ничья
+*/
 function check() {
     if ($('.kletka1').text() != '' && $('.kletka2').text() != '' && $('.kletka3').text() != '' && $('.kletka4').text() != '' && $('.kletka5').text() != '' && $('.kletka6').text() != '' && $('.kletka7').text() != '' && $('.kletka8').text() != '' && $('.kletka9').text() != ''){
         $('.result').text("Ничья"); 
@@ -269,6 +329,11 @@ function check() {
     }
 }
 
+/**
+*Генерация счёта игры
+*
+*@return {integer} счёт
+*/
 function score(){
     $('.score').text(score_user + " : " + score_comp); 
 }
