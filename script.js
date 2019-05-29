7d1de70dc412346ad0559a802fd84c985ab6bb46
AnnaSerozhenko <annieserezhenko@gmail.com>
2019-05-29 12:02:59 +0300

    Редактирование кода

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
-var znak_user = 'X';
-var znak_comp = 'O';
+/*
+ * Знаки, которыми играет игрок и компьютер
+ */
+var ZnakUser = 'X';
+var ZnakComp = 'O';
 
-var score_user = '0';
-var score_comp = '0';
+/*
+ * Обнуление счёта выигрышей
+*/
+var ScoreUser = '0';
+var ScoreComp = '0';
 
 /**
  * Инициализация
@@ -14,8 +20,8 @@ $(document).ready(function(){
      */
     document.body.style.zoom = "130%";
 
-    var exit_flag = false;
-    var win_user_array = ['123','456','789','147','258','369','159','357'];
+    var ExitFlag = false;
+    var WinUserArray = ['123','456','789','147','258','369','159','357'];
          
     /**
     *Определяем победу игрока
@@ -26,9 +32,9 @@ $(document).ready(function(){
     function check3user(znak){
         for (var i = 0; i < 8; i++) {
          
-            var first = 'kletka' + win_user_array[i].substr(0,1);
-            var second = 'kletka' + win_user_array[i].substr(1,1);
-            var third = 'kletka' + win_user_array[i].substr(2,1);
+            var first = 'kletka' + WinUserArray[i].substr(0,1);
+            var second = 'kletka' + WinUserArray[i].substr(1,1);
+            var third = 'kletka' + WinUserArray[i].substr(2,1);
              
              /**
              * Проверка выигрыша игрока
@@ -38,9 +44,9 @@ $(document).ready(function(){
                 $('.'+first+',.'+second+',.'+third).css("background-color", "#83e2c3");
                 $('.result').text('Вы выиграли!');
                 $('.MAIN_DIV .div').unbind('click');
-                score_user++;
+                ScoreUser++;
                 score();
-                exit_flag = true;
+                ExitFlag = true;
             }    
         }
     }
@@ -54,41 +60,41 @@ $(document).ready(function(){
     function check2comp(znak){
         for (var i = 0; i < 8; i++) {
          
-            var first = 'kletka' + win_user_array[i].substr(0,1);
-            var second = 'kletka' + win_user_array[i].substr(1,1);
-            var third = 'kletka' + win_user_array[i].substr(2,1);
+            var first = 'kletka' + WinUserArray[i].substr(0,1);
+            var second = 'kletka' + WinUserArray[i].substr(1,1);
+            var third = 'kletka' + WinUserArray[i].substr(2,1);
              
              /**
              * Проверка выигрыша коспьютера
              */ 
-            if( $('.'+first).text() == znak && $('.'+second).text() == znak && $('.'+third).text() == '' && exit_flag == false ){
+            if( $('.'+first).text() == znak && $('.'+second).text() == znak && $('.'+third).text() == '' && ExitFlag == false ){
                 $('.'+third).text(znak);
                 $('.'+first+',.'+second+',.'+third).css("background-color", "#EF7C7C");
                 $('.result').text('Вы проиграли!');
                 $('.MAIN_DIV .div').unbind('click');
-                score_comp++;
+                ScoreComp++;
                 score();
-                exit_flag = true;
+                ExitFlag = true;
             }    
              
-            if( $('.'+first).text() == znak && $('.'+second).text() == '' && $('.'+third).text() == znak && exit_flag == false ){
+            if( $('.'+first).text() == znak && $('.'+second).text() == '' && $('.'+third).text() == znak && ExitFlag == false ){
                 $('.'+second).text(znak);
                 $('.'+first+',.'+second+',.'+third).css("background-color", "#EF7C7C");
                 $('.result').text('Вы проиграли!');
                 $('.MAIN_DIV .div').unbind('click');
-                score_comp++;
+                ScoreComp++;
                 score();
-                exit_flag = true;
+                ExitFlag = true;
             }    
              
-            if( $('.'+first).text() == '' && $('.'+second).text() == znak && $('.'+third).text() == znak && exit_flag == false ){
+            if( $('.'+first).text() == '' && $('.'+second).text() == znak && $('.'+third).text() == znak && ExitFlag == false ){
                 $('.'+first).text(znak);
                 $('.'+first+',.'+second+',.'+third).css("background-color", "#EF7C7C");
                 $('.result').text('Вы проиграли!');
                 $('.MAIN_DIV .div').unbind('click');
-                score_comp++;
+                ScoreComp++;
                 score();
-                exit_flag = true;
+                ExitFlag = true;
             }    
         }
     }
@@ -102,31 +108,32 @@ $(document).ready(function(){
  
         for (var i = 0; i < 8; i++) {
          
-            var first = 'kletka' + win_user_array[i].substr(0,1);
-            var second = 'kletka' + win_user_array[i].substr(1,1);
-            var third = 'kletka' + win_user_array[i].substr(2,1);
+            var first = 'kletka' + WinUserArray[i].substr(0,1);
+            var second = 'kletka' + WinUserArray[i].substr(1,1);
+            var third = 'kletka' + WinUserArray[i].substr(2,1);
  
              
-            if( exit_flag == false ){
+            if( ExitFlag == false ){
                 if( $('.'+first).text() == znak && $('.'+second).text() == znak && $('.'+third).text() == '' ){
-                    $('.'+third).text(znak_comp);
-                    exit_flag = true;
+                    $('.'+third).text(ZnakComp);
+                    ExitFlag = true;
                 }
             }
                          
-            if( exit_flag == false ){
+            if( ExitFlag == false ){
                 if( $('.'+first).text() == znak && $('.'+second).text() == '' && $('.'+third).text() == znak ){
-                    $('.'+second).text(znak_comp);
-                    exit_flag = true;
+                    $('.'+second).text(ZnakComp);
+                    ExitFlag = true;
                 }    
             }    
              
             if( $('.'+first).text() == '' && $('.'+second).text() == znak && $('.'+third).text() == znak ){
-                $('.'+first).text(znak_comp);
-                exit_flag = true;
+                $('.'+first).text(ZnakComp);
+                ExitFlag = true;
             }
             
-            if(exit_flag) break;
+            if(ExitFlag)
+                break;
         }
     }
      
@@ -136,19 +143,20 @@ $(document).ready(function(){
     $('.MAIN_DIV .div').click(function(){
         //Если клетка пустая
         if( $(this).text() == '' ){
-            $(this).text(znak_user); 
+            $(this).text(ZnakUser); 
             check();   
-            check3user(znak_user);
-            check2comp(znak_comp);
-            check2user(znak_user);
-            if( exit_flag == false ){
+            check3user(ZnakUser);
+            check2comp(ZnakComp);
+            check2user(ZnakUser);
+            if( ExitFlag == false ){
                 for (var i = 1; i < 10; i++) {    
                     if( $('.kletka'+i).text() == '' ){
-                        $('.kletka'+i).text(znak_comp);
+                        $('.kletka'+i).text(ZnakComp);
                         break;
                     }    
                 }
-            }else exit_flag = false;
+            }else
+                ExitFlag = false;
         }
     });
 });
@@ -162,29 +170,28 @@ function restart(){
     $('.kletka1, .kletka2, .kletka3, .kletka4, .kletka5, .kletka6, .kletka7, .kletka8, .kletka9').css("background-color", "#FFF");
     $( ".MAIN_DIV .div" ).bind("click", function (){
  
-        /**
-        * Если клетка пустая
-        */ 
+        //Если клетка пустая
         if( $(this).text() == '' ){
-            $(this).text(znak_user);    
-            check3user(znak_user);
-            check2comp(znak_comp);
-            check2user(znak_user);
+            $(this).text(ZnakUser);    
+            check3user(ZnakUser);
+            check2comp(ZnakComp);
+            check2user(ZnakUser);
              
-            if( exit_flag == false ){
+            if( ExitFlag == false ){
                 for (var i = 1; i < 10; i++) {    
                     if( $('.kletka'+i).text() == '' ){
-                        $('.kletka'+i).text(znak_comp);
+                        $('.kletka'+i).text(ZnakComp);
                         break;
                     }    
                 }
-            }else exit_flag = false;
+            }else
+                ExitFlag = false;
         }
     });
-} 
+}
 
-var exit_flag = false;
-var win_user_array = ['123','456','789','147','258','369','159','357'];
+var ExitFlag = false;
+var WinUserArray = ['123','456','789','147','258','369','159','357'];
      
 /**
 *Определяем победу игрока
@@ -195,21 +202,18 @@ var win_user_array = ['123','456','789','147','258','369','159','357'];
 function check3user(znak){
     for (var i = 0; i < 8; i++) {
         check();
-        var first = 'kletka' + win_user_array[i].substr(0,1);
-        var second = 'kletka' + win_user_array[i].substr(1,1);
-        var third = 'kletka' + win_user_array[i].substr(2,1);
+        var first = 'kletka' + WinUserArray[i].substr(0,1);
+        var second = 'kletka' + WinUserArray[i].substr(1,1);
+        var third = 'kletka' + WinUserArray[i].substr(2,1);
          
-         /**
-         * Проверка выигрыша игрока
-         */ 
         if( $('.'+first).text() == znak && $('.'+second).text() == znak && $('.'+third).text() == znak ){
             $('.kletka1, .kletka2, .kletka3, .kletka4, .kletka5, .kletka6, .kletka7, .kletka8, .kletka9').css("background-color", "#FFF");
             $('.'+first+',.'+second+',.'+third).css("background-color", "#83e2c3");
             $('.result').text('Вы выиграли!');
             $('.MAIN_DIV .div').unbind('click');
-            score_user++;
+            ScoreUser++;
             score();
-            exit_flag = true;
+            ExitFlag = true;
         }     
     }
 }
@@ -223,41 +227,41 @@ function check3user(znak){
 function check2comp(znak){
     for (var i = 0; i < 8; i++) {
      
-        var first = 'kletka' + win_user_array[i].substr(0,1);
-        var second = 'kletka' + win_user_array[i].substr(1,1);
-        var third = 'kletka' + win_user_array[i].substr(2,1);
+        var first = 'kletka' + WinUserArray[i].substr(0,1);
+        var second = 'kletka' + WinUserArray[i].substr(1,1);
+        var third = 'kletka' + WinUserArray[i].substr(2,1);
          
         /**
         * Проверка выигрыша коспьютера
         */ 
-        if( $('.'+first).text() == znak && $('.'+second).text() == znak && $('.'+third).text() == '' && exit_flag == false ){
+        if( $('.'+first).text() == znak && $('.'+second).text() == znak && $('.'+third).text() == '' && ExitFlag == false ){
             $('.'+third).text(znak);
             $('.'+first+',.'+second+',.'+third).css("background-color", "#EF7C7C");
             $('.result').text('Вы проиграли!');
             $('.MAIN_DIV .div').unbind('click');
-            score_user++;
+            ScoreComp++;
             score();
-            exit_flag = true;
+            ExitFlag = true;
         }    
          
-        if( $('.'+first).text() == znak && $('.'+second).text() == '' && $('.'+third).text() == znak && exit_flag == false ){
+        if( $('.'+first).text() == znak && $('.'+second).text() == '' && $('.'+third).text() == znak && ExitFlag == false ){
             $('.'+second).text(znak);
             $('.'+first+',.'+second+',.'+third).css("background-color", "#EF7C7C");
             $('.result').text('Вы проиграли!');
             $('.MAIN_DIV .div').unbind('click');
-            score_user++;
+            ScoreComp++;
             score();
-            exit_flag = true;
+            ExitFlag = true;
         }    
          
-        if( $('.'+first).text() == '' && $('.'+second).text() == znak && $('.'+third).text() == znak && exit_flag == false ){
+        if( $('.'+first).text() == '' && $('.'+second).text() == znak && $('.'+third).text() == znak && ExitFlag == false ){
             $('.'+first).text(znak);
             $('.'+first+',.'+second+',.'+third).css("background-color", "#EF7C7C");
             $('.result').text('Вы проиграли!');
             $('.MAIN_DIV .div').unbind('click');
-            score_user++;
+            ScoreComp++;
             score();
-            exit_flag = true;
+            ExitFlag = true;
         }    
     }
 }
@@ -271,30 +275,31 @@ function check2user(znak){
 
     for (var i = 0; i < 8; i++) {
      
-        var first = 'kletka' + win_user_array[i].substr(0,1);
-        var second = 'kletka' + win_user_array[i].substr(1,1);
-        var third = 'kletka' + win_user_array[i].substr(2,1);
+        var first = 'kletka' + WinUserArray[i].substr(0,1);
+        var second = 'kletka' + WinUserArray[i].substr(1,1);
+        var third = 'kletka' + WinUserArray[i].substr(2,1);
 
          
-        if( exit_flag == false ){
+        if( ExitFlag == false ){
             if( $('.'+first).text() == znak && $('.'+second).text() == znak && $('.'+third).text() == '' ){
-                $('.'+third).text(znak_comp);
-                exit_flag = true;
+                $('.'+third).text(ZnakComp);
+                ExitFlag = true;
             }
         }
                      
-        if( exit_flag == false ){
+        if( ExitFlag == false ){
             if( $('.'+first).text() == znak && $('.'+second).text() == '' && $('.'+third).text() == znak ){
-                $('.'+second).text(znak_comp);
-                exit_flag = true;
+                $('.'+second).text(ZnakComp);
+                ExitFlag = true;
             }    
         }    
          
         if( $('.'+first).text() == '' && $('.'+second).text() == znak && $('.'+third).text() == znak ){
-            $('.'+first).text(znak_comp);
-            exit_flag = true;
+            $('.'+first).text(ZnakComp);
+            ExitFlag = true;
         }
-        if(exit_flag) break;
+        if(ExitFlag)
+            break;
     }
 }
 
@@ -302,20 +307,22 @@ function check2user(znak){
 *Обработка клика ☺ + ☻™ = ♥♥♥
 */ 
 $('.MAIN_DIV .div').click(function(){
+    //Если клетка пустая
     if( $(this).text() == '' ){
-        $(this).text(znak_user); 
+        $(this).text(ZnakUser); 
         check();   
-        check3user(znak_user);
-        check2comp(znak_comp);
-        check2user(znak_user);  
-        if( exit_flag == false ){
+        check3user(ZnakUser);
+        check2comp(ZnakComp);
+        check2user(ZnakUser);  
+        if( ExitFlag == false ){
             for (var i = 1; i < 10; i++) {    
                 if( $('.kletka'+i).text() == '' ){
-                    $('.kletka'+i).text(znak_comp);
+                    $('.kletka'+i).text(ZnakComp);
                     break;
                 }
             }
-        }else exit_flag = false;
+        }else 
+            ExitFlag = false;
     }
 });
 
@@ -335,5 +342,5 @@ function check() {
 *@return {integer} счёт
 */
 function score(){
-    $('.score').text(score_user + " : " + score_comp); 
+    $('.score').text(ScoreUser + " : " + ScoreComp); 
 }
